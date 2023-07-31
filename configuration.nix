dd7c1d03bc39dd41d6af0633c61f091fa72759a9
Joep <jcbuhre@gmail.com>
2023-07-31 21:56:46 +0200

    add import

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      ./traefik.nix
     ];
 
   system.autoUpgrade.channel = "https://nixos.org/channels/nixos-23.05/";
